e6a4f2cfb5bd918e04db3508b1de9639d3588dd0
3Nina <ninamuhafara@gmail.com>
2025-05-30 10:31:04 -0300

    Anda el conteo de plata

---
+++ Assets/MercanciaScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MercanciaScript : MonoBehaviour
+{
+    public float cost = 5;
+    public MoneyManager moneyManager; // para conectarse con el money manager del player
+
+     void Start()
+    {
+        moneyManager = FindObjectOfType<MoneyManager>(); // busca en la escena un componente del tipo entre <  >. Si hay mas de uno agarra el priemro que encuentra entonces solo se usa cuando hay solo un tipo de este elemento en la escena.
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))  // compara tags porque es más eficiente. para que ande hay que cambiar la tag de player
+        {
+            moneyManager.UpdateMoney(-cost); // llama a la función que creamos --> UpdateMoney en el script moneyManager. 
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/MoneyManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour
+{
+    public float money;
+
+    public void UpdateMoney(float amount)
+    {
+        if (money + amount < 0) // si la cant de plata es menor a la compra no deja comprar. Premite vender si la venta vale mas que la cant de money.
+        {
+            // impedir la compra
+            Debug.Log("Dinero insuficiente");
+        } else
+        {
+            money += amount;  // le suma la cant de money a amount y se guara (actualiza) el valor de la variable money
+        }
+        
+    }
+}
